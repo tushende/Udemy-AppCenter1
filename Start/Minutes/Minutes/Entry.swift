41e4215a3dc3186a55baf1921730d3ab7347d735
Tushar Shende <tushar.shende@capgemini.com>
2019-07-15 16:52:14 +0530

    Just added .

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "Hello"
-    var content: String = "There"
+    var content: String = "There."
     
     var id: String = UUID().uuidString
     
